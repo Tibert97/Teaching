ad2a6bb4993703776543a9c6fd6484f25b0bebd7
Timo Bertram <tb.phoenix@t-online.de>
2021-04-26 19:34:03 +0200

    Initial commit

---
+++ Rekursion/Permutation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Permutation {
+	public static void main(String[] args) {
+		System.out.println("Enter your word.");
+		Scanner scanner = new Scanner(System.in);
+		String word = scanner.nextLine();
+		System.out.println("All possible permutations are:");
+		permutateWord("",word);
+		permutateWordIterative(word);
+		scanner.close();
+
+	}
+	
+	public static void permutateWord(String firstLetters, String word) {
+		if(word.length() == 0) {
+			System.out.println(firstLetters);
+		}
+		else {
+			for(int i = 0; i <= word.length()-1; i++) {
+				String restOfWord = word.substring(0,i)+word.substring(i+1);
+				permutateWord(firstLetters+word.charAt(i),restOfWord);
+			}	
+		}
+		return;
+	}
+
+}
